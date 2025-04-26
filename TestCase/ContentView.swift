76d4d928814fef34714754cdf08b34647bbc4486
Hilal İnan <hilalinan61@gmail.com>
2025-04-26 11:27:02 +0300

    Initial Commit

---
+++ TestCase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestCase
+//
+//  Created by Hilal İnan on 26.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestCase/TestCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCaseApp.swift
+//  TestCase
+//
+//  Created by Hilal İnan on 26.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
